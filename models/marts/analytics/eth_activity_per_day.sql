df641a8b6aa88e53cde9683eafaed4bd3dd6d09d
jbruce0308 <jessebruce035@gmail.com>
2026-06-08 11:47:28 -0400

    new pr to test slimci

--- models/marts/analytics/eth_activity_per_day.sql
+++ models/marts/analytics/eth_activity_per_day.sql
@@ -2,7 +2,7 @@ select
 date,
 transaction_category,
 count(*) as tx_count,
-{{ conversion('value','18') }} as sum_value_etherum
+{{ conversion('value','18') }} as sum_value_etherum_v2
 from {{ ref('stg_transactions_enriched') }} as eth_value
 group by 
 date,
